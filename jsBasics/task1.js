17935c8c82979608ea238f3a42b52d40d2975912
Maksym <maksym.tserna@gmail.com>
2026-04-28 17:20:59 +0300

    Add JS homework 4

---
+++ jsBasics/task1.js
@@ -0,0 +1,17 @@
+// Оголошення змінних для примітивних типів
+
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: рядок, число, 
+// буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+let text = "строка";
+let count = 39;
+let isMe = true;
+let nothing = null;
+let undef = undefined;
+
+console.log(text);
+console.log(count);
+console.log(isMe);
+console.log(nothing);
+console.log(undef);
+
---
+++ jsBasics/task4.js
@@ -0,0 +1,38 @@
+// Завдання 4.1
+
+// π - число “пі”. Ви можете використати Math.PI у вашому дз для вираження цього числа
+
+// Створіть змінну radius і присвойте їй числове значення радіуса кола.
+// Обчисліть площу кола за формулою π * radius^2 і виведіть результат.
+
+let radius = 12;
+
+let squareCircle = Math.PI * radius ** 2;
+
+console.log(`Площа круга дорівнює ${squareCircle.toFixed(2)}`);
+
+// Завдання 4.2
+
+// Створіть змінну length і присвойте їй числове значення довжини прямокутника.
+// Створіть змінну width і присвойте їй числове значення ширини прямокутника.
+// Обчисліть площу прямокутника за формулою length * width і виведіть результат.
+
+let lengthRectangle = 21;
+let widthRectangle = 11;
+
+let squareRectangle = lengthRectangle * widthRectangle;
+
+console.log(`Площа прямокутники виходить що ${squareRectangle.toFixed(2)}`);
+
+// Завдання 4.3
+
+// Створіть змінну radius і присвойте їй числове значення радіуса циліндра.
+// Створіть змінну height і присвойте їй числове значення висоти циліндра.
+// Обчисліть об'єм циліндра за формулою π * radius^2 * height і виведіть результат.
+
+let radiusCylinder = 33;
+let heightCylinder = 19;
+
+let volumeCylinder = Math.PI * radiusCylinder ** 2 * heightCylinder;
+
+console.log(`Обʼєм циліндру виходить що ${volumeCylinder.toFixed(2)}`);
