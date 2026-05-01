c3f55e69430ae629c1dfb6a6e453301c2c81d84d
Maksym <maksym.tserna@gmail.com>
2026-05-01 17:46:47 +0300

    Add JS conditions and loops homework

---
+++ jsConditions/task3.js
@@ -0,0 +1,14 @@
+const number = 5;
+
+// FOR
+for (let i = 1; i <= 10; i++) {
+    console.log(`${number} x ${i} = ${number * i}`);
+}
+
+// WHILE
+let i = 1;
+
+while (i <= 10) {
+    console.log(`${number} x ${i} = ${number * i}`);
+    i++;
+}
